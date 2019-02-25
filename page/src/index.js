2bed7c2ad1647701e004c1d4d93f8a5aa45cdb07
Niels <niels.reijnders@edu-kw1c.nl>
2019-02-25 11:57:49 +0100

    commit

--- page/src/index.js
+++ page/src/index.js
@@ -14,9 +14,31 @@ nav.addEventListener("click", function(e) {
 
     } else {
 
-        element.className = "";
+        element.classList.remove("expand");
         element.style.overflow = "visible";
 
         expand = true;
+    }
+})
+
+var cart = document.querySelectorAll(".cart")[0];
+
+var expand2 = true;
+
+cart.addEventListener("click", function(e) {
+    e.preventDefault;
+    if (expand2) {
+
+        element.className += " cart";
+        element.style.overflow = "hidden";
+
+        expand2 = false;
+
+    } else {
+
+        element.classList.remove("cart");
+        element.style.overflow = "visible";
+
+        expand2 = true;
     }
 })
